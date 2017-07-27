06d9f8801b0e2c8cbad9e804b1b993540be1cffe
unknown <jonen54@gmail.com>
2017-07-28 00:39:00 +0300

    [CHORE] Added application starting point

---
+++ run.py
@@ -0,0 +1,10 @@
+import os
+from app import create_app
+
+""" This module defines the application's start point"""
+
+config_name=os.getenv('APP_SETTINGS') #loads the configuration name from the environment variable
+app=create_app(config_name)
+
+if __name__=='__main__':
+	app.run()
