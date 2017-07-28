70488d7051b70a85839a699d8934eb61f9ffe3a8
unknown <jonen54@gmail.com>
2017-07-28 08:35:26 +0300

    Added docstring description

---
+++ manage.py
@@ -0,0 +1,19 @@
+import os
+from flask_script import Manager
+from flask_migrate import Migrate,MigrateCommand
+from app import db,create_app
+from app.models import users,bucketlists,bucketlistItems
+
+"""
+	Migrations is a way of propagating changes made to models into the database schema
+	Manager class keeps track of all the commands and handles how they are called from the commandline
+"""
+
+app=create_app(config_name=os.getenv("APP_SETTINGS"))
+migrate=Migrate(app,db)
+manager=Manager(app)
+
+manager.add_command('db',MigrateCommand) #MigrateCommand contains a set of migration commands
+
+if __name__=="__main__": 
+	manager.run()
