93148587b4b569d549d61c62ec659e6d502f4336
unknown <jonen54@gmail.com>
2017-07-29 13:26:41 +0300

    Edited test url enpoints

--- tests/test_user_crud.py
+++ tests/test_user_crud.py
@@ -2,6 +2,7 @@ import unittest
 import os
 import json
 from app import create_app,db
+import json
 
 class TestUserCrud(unittest.TestCase):
 	def setUp(self):
@@ -9,7 +10,7 @@ class TestUserCrud(unittest.TestCase):
 		   A test database is also initialised to store the test data 
 		"""
 		self.app=create_app(config_name="testing")
-		self.client=self.app.test_client
+		self.client=self.app.test_client(self)
 
 		with self.app.app_context(): #associate the app with the current context
 			db.create_all() #creates all required tables
@@ -18,59 +19,60 @@ class TestUserCrud(unittest.TestCase):
 
 	def test_user_is_created(self):
 		""" Test if a user is created """
-		result=self.client().post("/auth/register",data=self.test_user)
+		result=self.client.post("api/v1/auth/register",data=self.test_user)
 		self.assertEqual(result.status_code,201)
-		self.assertDictEqual(result.data,{"status":"success","message":"User registered successfully"},"User registration failed")
+		res=json.loads(result.data)
+		self.assertDictEqual(res,{"status":"success","message":"User registered successfully"},"User registration failed")
 
 	def test_duplicate_user_is_created(self):
-		""" Test if a user is created """
-		result=self.client().post("/auth/register",data=self.test_user)
+		""" Test if a user is duplicated """
+		result=self.client.post("api/v1/auth/register",data=self.test_user)
 		self.assertEqual(result.status_code,201)
-		duplicate=self.client().post("/auth/register",data=self.test_user)
+		duplicate=self.client.post("api/v1/auth/register",data=self.test_user)
 		self.assertEqual(duplicate.status_code,409,"Duplicates are being added to the db")
-		self.assertDictEqual(duolicate.data,{"status":"success","message":"User registered successfully"},"User registration failed")
+		self.assertDictEqual(json.loads(duplicate.data),{"status":"success","message":"User registered successfully"},"User registration failed")
 
 	def test_user_logs_in(self):
 		"""Test if a user is able to login successfully"""
-		result=self.client().post("/auth/register",data=self.test_user)
+		result=self.client.post("api/v1/auth/register",data=self.test_user)
 		self.assertEqual(result.status_code,201,"User not registered")
-		login=self.client().post("/auth/login",data=self.login_data)
+		login=self.client.post("api/v1/auth/login",data=self.login_data)
 		self.assertEqual(login.status_code,201,"User not logged in")
-		self.assertDictEqual(login.data,{"status":"success","message":"Login was successful"},"User failed to login")
+		self.assertDictEqual(json.loads(login.data),{"status":"success","message":"Login was successful"},"User failed to login")
 
 	def test_user_logs_out(self):
 		"""Test if a user is able to logout"""
-		result=self.client().post("/auth/register",data=self.test_user)
+		result=self.client.post("api/v1/auth/register",data=self.test_user)
 		self.assertEqual(result.status_code,201,"user not registered successfully")
-		login=self.client().post("/auth/login",data=self.login_data)
+		login=self.client.post("api/v1/auth/login",data=self.login_data)
 		self.assertEqual(login.status_code,201,"User login failed")
-		logout=self.client().get("/auth/logout")
+		logout=self.client.get("api/v1/auth/logout")
 		self.assertEqual(logout.status_code,200,"User logged out successfully")
-		self.assertDictEqual(logout.data,{"status":"successful","message":"User logged out successfully"})
+		self.assertDictEqual(json.loads(logout.data),{"status":"successful","message":"User logged out successfully"})
 
 	def test_user_reset_password(self):
 		"""testing if user is able to reset password"""
-		result=self.client().post("/auth/register",data=self.test_user)
+		result=self.client.post("api/v1/auth/register",data=self.test_user)
 		self.assertEqual(result.status_code,201,"user not registered successfully")
-		login=self.client().post("/auth/login",data=self.login_data)
+		login=self.client.post("api/v1/auth/login",data=self.login_data)
 		self.assertEqual(login.status_code,201,"User login failed")
 		reset_data={"old_password":"256thjuly","new_password":"257thjuly"}
-		reset_password=self.client().post("/auth/password_rest",data=reset_data)
+		reset_password=self.client.post("api/v1/auth/password_rest",data=reset_data)
 		self.assertEqual(reset_password.status_code,201,"Passsword was not reset")
 		new_login_data={"email":"jonen54@gmail.com","password":"257thjuly"}
-		login_after_reset=self.client().post("/auth/login",data=self.new_login_data)
+		login_after_reset=self.client.post("api/v1/auth/login",data=self.new_login_data)
 		self.assertEqual(login_after_reset.status_code,201,"Login was not successful after reset")
 
 	def test_login_after_password_reset(self):
 		"""testing if user is able to login using the previous password after a password reset"""
-		result=self.client().post("/auth/register",data=self.test_user)
+		result=self.client.post("api/v1/auth/register",data=self.test_user)
 		self.assertEqual(result.status_code,201,"user not registered successfully")
-		login=self.client().post("/auth/login",data=self.login_data)
+		login=self.client.post("api/v1/auth/login",data=self.login_data)
 		self.assertEqual(login.status_code,201,"User login failed")
 		reset_data={"old_password":"256thjuly","new_password":"257thjuly"}
-		reset_password=self.client().post("/auth/password_rest",data=reset_data)
+		reset_password=self.client.post("api/v1/auth/password_rest",data=reset_data)
 		self.assertEqual(reset_password.status_code,201,"Passsword was not reset")
-		login_after_reset=self.client().post("/auth/login",data=self.login_data)
+		login_after_reset=self.client.post("api/v1/auth/login",data=self.login_data)
 		self.assertEqual(login_after_reset.status_code,401,"Login was successful after reset, password was not reset")
 
 	def tearDown(self):
