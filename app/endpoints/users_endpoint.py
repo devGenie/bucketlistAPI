b5fd791ef17629de3485bde5563aaefacfee2873
unknown <jonen54@gmail.com>
2017-07-30 01:15:26 +0300

    Passing tests for duplicates

--- app/endpoints/users_endpoint.py
+++ app/endpoints/users_endpoint.py
@@ -2,6 +2,7 @@ from app.restplus import api
 from flask_restplus import Resource
 from app.models.users import Users as User
 from flask import request
+from app.database import db
 
 
 ns=api.namespace("auth",description="Use these endpoints to create user accounts and login into the application")
@@ -10,11 +11,21 @@ ns=api.namespace("auth",description="Use these endpoints to create user accounts
 class Register(Resource):
 	"""Register user into the database"""
 	def post(self):
-		first_name=request.data['first_name']
-		last_name=request.data['last_name']
-		email=request.data['email']
-		password=request.data['password']
-		user=User(first_name,last_name,email,password)
-		user.save()
-		data={"fname":first_name,"lname":last_name,"user_id":user.id}
-		return data,201
+		my_email=request.data['email']
+		exists=db.session.query(db.exists().where(User.email==my_email)).scalar()
+		if exists:
+			data={"status":"failed","message":"User registration failed,email already extsis"}
+			return data,409
+		else:
+			first_name=request.data['first_name']
+			last_name=request.data['last_name']
+			password=request.data['password']
+			user=User(first_name,last_name,my_email,password)
+			user.save()
+			if user.id:
+				data={"status":"success","message":"User registered successfully"}
+				return data,201
+			else:
+				data={"status":"failed","message":"User registration failed"}
+				return data,400
+
