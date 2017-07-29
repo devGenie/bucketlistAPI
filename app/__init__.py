864bb7011bc12fd57c2ea7f47601e428eb6cd9ff
unknown <jonen54@gmail.com>
2017-07-29 13:26:02 +0300

    Edited api endpoint

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,8 @@
 from flask_api import FlaskAPI
 from flask_sqlalchemy import SQLAlchemy
+from flask import Blueprint
+from app.restplus import api
+from app.endpoints.users_endpoint import ns as users_namespace
 
 #local import, import configurations
 from instance.config import app_config
@@ -10,10 +13,15 @@ def create_app(config_name):
 	""" This function wraps the creation of a new flask object and returns it after
 		all the configurations have been loaded and db connections have been made
 	"""
+	blueprint=Blueprint("api",__name__,url_prefix='/api/v1')
+	api.init_app(blueprint)
+	api.add_namespace(users_namespace)
+
 	app=FlaskAPI(__name__,instance_relative_config=True)
 	app.config.from_object(app_config[config_name]) #loads the configuration from imported dictionary
 	app.config.from_pyfile('config.py')
 	app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+	app.register_blueprint(blueprint)
 	db.init_app(app)
 
 	return app
