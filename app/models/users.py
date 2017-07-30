3a26be37a94c62dedd02165effe7e80adbd27ed5
unknown <jonen54@gmail.com>
2017-07-30 13:14:28 +0300

    [CHORE] Added token generator

--- app/models/users.py
+++ app/models/users.py
@@ -1,6 +1,8 @@
 from app.database import db
 from app.models.bucketlists import Bucketlists
 import bcrypt
+import os
+from itsdangerous import (TimedJSONWebSignatureSerializer as serializer,BadSignature,SignatureExpired)
 
 class Users(db.Model):
 	"""This class represents the user table"""
@@ -27,6 +29,11 @@ class Users(db.Model):
 		"""This method deletes the record from the user's table"""
 		db.session.delete(self)
 		db.session.commit()
+
+	def generate_auth(self,expires=600):
+		secret=os.getenv("SECRET")
+		serialized=serializer(secret,expires_in=expires)
+		return serialized.dumps({"id":self.id})
 
 	def __repr__(self):
 		return {"user_id":self.id,"email":self.email,"first_name":self.first_name,"last_name":self.last_name}
