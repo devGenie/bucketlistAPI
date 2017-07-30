5b611a83c5e27f551418573683af9a0995bb640c
unknown <jonen54@gmail.com>
2017-07-30 19:00:51 +0300

    Added table for blacklisting tokens

---
+++ app/models/blacklist.py
@@ -0,0 +1,15 @@
+from app.database import db
+
+class BlackList(db.Model):
+	""" Define a table that keeps record of blacklisted tokens on logout"""
+	__tablename__="blacklist"
+	id=db.Column(db.Integer(),primary_key=True)
+	token=db.Column(db.String(255),nullable=False)
+	blacklisted_on=db.Column(db.DateTime,nullable=False,default=datetime.datetime.now())
+
+	def __init__(self,token):
+		self.token=token
+
+	def save(self):
+		db.session.add(self)
+		db.session.commit()
