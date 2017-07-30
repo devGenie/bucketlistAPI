6000d3d62c95f288d76db78226ea034fce6e3c1a
unknown <jonen54@gmail.com>
2017-07-30 10:48:28 +0300

    [FEATURE] Implemented user login

--- app/endpoints/users_endpoint.py
+++ app/endpoints/users_endpoint.py
@@ -3,29 +3,52 @@ from flask_restplus import Resource
 from app.models.users import Users as User
 from flask import request
 from app.database import db
+import bcrypt
 
 
-ns=api.namespace("auth",description="Use these endpoints to create user accounts and login into the application")
+ns = api.namespace(
+    "auth", description="Use these endpoints to create user accounts and login into the application")
+
 
 @ns.route("/register")
 class Register(Resource):
-	"""Register user into the database"""
+    """Register user into the database"""
+
+    def post(self):
+        my_email = request.data['email']
+        exists = db.session.query(db.exists().where(User.email == my_email)).scalar()
+        if exists:
+            data = {"status": "failed",
+                    "message": "User registration failed,email already extsis"}
+            return data, 409
+        else:
+            first_name = request.data['first_name']
+            last_name = request.data['last_name']
+            password = request.data['password']
+            user = User(first_name, last_name, my_email, password)
+            user.save()
+            if user.id:
+                data = {"status": "success",
+                        "message": "User registered successfully"}
+                return data, 201
+            else:
+                data = {"status": "failed",
+                        "message": "User registration failed"}
+                return data, 400
+
+@ns.route("/login")
+class Login(Resource):
+	"""Log user into the application and produce a token that will be used for authentication"""
 	def post(self):
 		my_email=request.data['email']
-		exists=db.session.query(db.exists().where(User.email==my_email)).scalar()
-		if exists:
-			data={"status":"failed","message":"User registration failed,email already extsis"}
-			return data,409
+		password=request.data['password'].encode("utf8")
+		hashed=bcrypt.hashpw(password,bcrypt.gensalt())
+
+		get_user=User.query.filter_by(email=my_email).first()
+		if get_user:
+			if bcrypt.checkpw(password,get_user.password):
+				data={"status":"success","message":"Login was successful"}
+				return data,202
 		else:
-			first_name=request.data['first_name']
-			last_name=request.data['last_name']
-			password=request.data['password']
-			user=User(first_name,last_name,my_email,password)
-			user.save()
-			if user.id:
-				data={"status":"success","message":"User registered successfully"}
-				return data,201
-			else:
-				data={"status":"failed","message":"User registration failed"}
-				return data,400
+			print(get_user)
 
