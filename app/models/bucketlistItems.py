c8a5f6c3d181c0d804e48f76eb122c92d9360747
devGenie <jonen54@gmail.com>
2017-08-03 01:57:56 +0300

    Changed date completed datatype

--- app/models/bucketlistItems.py
+++ app/models/bucketlistItems.py
@@ -1,4 +1,5 @@
 from app.database import db
+import datetime
 
 class BucketlistItems(db.Model):
 	"""This class models the bucket list itens table"""
@@ -6,7 +7,7 @@ class BucketlistItems(db.Model):
 	id=db.Column(db.Integer,primary_key=True)
 	name=db.Column(db.String(20),nullable=False)
 	date_added=db.Column(db.DateTime,default=db.func.current_timestamp())
-	date_completed=db.Column(db.DateTime,nullable=True)
+	date_completed=db.Column(db.String,nullable=True)
 	complete_status=db.Column(db.Boolean,default=False)
 	bucketlist=db.Column(db.Integer,db.ForeignKey("bucketlists.id",ondelete='CASCADE'),nullable=False)
 
@@ -23,7 +24,7 @@ class BucketlistItems(db.Model):
 
 	def complete(self):
 		self.complete_status=True
-		self.date_completed=db.func.current_timestamp().stftime("%b/%d/%y")
+		self.date_completed=datetime.datetime.now().strftime("%b/%d/%y")
 		db.session.commit()
 
 	def delete(self):
