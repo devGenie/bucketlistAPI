630821262bb649e72534d8381dc6fa0f75d37723
devGenie <jonen54@gmail.com>
2017-08-02 18:53:03 +0300

    [FEATURE] Implemented add bucketlist item endpoint

--- app/endpoints/bucketlist_item_endpoint.py
+++ app/endpoints/bucketlist_item_endpoint.py
@@ -2,17 +2,35 @@ from app.restplus import api
 from flask_restplus import Resource
 from app.models.users import Users
 from app.models.bucketlists import Bucketlists
+from app.models.bucketlistItems import BucketlistItems
 from app.endpoints.users_endpoint import authenticate
 from app.endpoints.bucketlists_endpoint import ns
 from flask import request
 
 ns=api.namespace("bucketlists",description="Use these endpoints to manipulate bucketlist data")
-@ns.route("/<int:bucketlist_id>/","/<int:bucketlist_id>/items/<int:bucketlist_item>/","/<int:bucketlist_id>/items/")
+@ns.route("/<int:bucketlist_id>/items/<int:bucketlist_item>","/<int:bucketlist_id>/items/")
 class BucketListItemCrud(Resource):
 	""" Perform crud operations on bucketlist items """
 	@authenticate
 	def post(self,user,bucketlist_id,*arg,**kwargs):
-		pass
+		name=request.data['name']
+		bucketlist=Bucketlists.query.filter_by(id=bucketlist_id,user=user.id).first()
+		initial_count=len(bucketlist.items)
+		bucketlist.add_item(name)
+		new_count=len(bucketlist.items)
+		if new_count>initial_count:
+			new_item=bucketlist.items[-1]
+			item_data={
+					   "id":new_item.id,
+					   "name":new_item.name,
+					   "date_added":new_item.date_added.strftime("%b/%d/%y"),
+					   "date_completed":new_item.date_completed,
+					   "complete_status":new_item.complete_status}
+			data={"status":"success","message":"Item added successfully","data":item_data}
+			return data,201
+		else:
+			data={"status":"failed","message":"Item not added successfully"}
+			return data,200
 
 	@authenticate
 	def get(self,user,bucketlist_id,bucketlist_item=None,*arg,**kwargs):
