76f7547a66d43f5012543a52b95de57fe8cb5a2d
devGenie <jonen54@gmail.com>
2017-08-26 07:44:19 +0300

    [CHORE] Add validation decorator

--- app/common/decorators.py
+++ app/common/decorators.py
@@ -19,3 +19,44 @@ def authenticate(func):
             return {"status":"failed","message":"Not authorised"},409
     inner_methos.__doc__=func.__doc__
     return inner_methos
+
+def validate(params):
+    def validate_wrapper(func):
+        wraps(func)
+
+        def inner_method(*args, **kwargs):
+            errors = {'missing': [], 'errors': []}
+            for key in params.keys():
+                if key in request.data:
+                    if params[key]['type'] == 'integer':
+                        regexp=re.compile(r"(^[0-9]*$#)")
+                        compare=request.data.get(key)
+                        if not regexp.match(compare):
+                            errors['errors'].append({'value':compare,'field type':key,'message':'Please provide a valid integer'})
+
+                    if params[key]['type'] == 'text':
+                        regexp=re.compile(r"(^[a-zA-Z0-9]*$)")
+                        compare=request.data.get(key)
+                        if not regexp.match(compare):
+                            errors['errors'].append({'value':compare,'field type':key,'message':'Please provide a valid string'})
+                        else:
+                            if params[key]['min-length'] and len(compare.strip()) < params[key]['min-length']:
+                                errors['errors'].append({'value':compare,'field type':key,'message':'Minimum length reached'})
+                                
+
+                    if params[key]['type'] == 'email':
+                        address = re.compile(
+                            r"(^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$)")
+                        compare=request.data.get(key)
+                        if not address.match(compare):
+                            errors['errors'].append({'value':compare,'field type':key,'message':'Please provide a valid email'})
+                else:
+                    errors['missing'].append(key)
+            if len(errors['missing'])>0 or len(errors['errors'])>0:
+                return {'status':'failed','message':'Please check your input','errors':errors},400
+            else:
+                return func(*args,**kwargs)
+
+        inner_method.__doc__ = func.__doc__
+        return inner_method
+    return validate_wrapper
