140d9189bf40a81b43a500b2abc103ca1ddcfd4d
Onen Julius <jonen54@gmail.com>
2018-01-16 03:24:05 +0300

    Add henkins configuration

--- run.py
+++ run.py
@@ -2,6 +2,7 @@
 import os
 from app import create_app
 
+# entry point for the application
 # loads the configuration name from the environment variable
 config_name = os.getenv('APP_SETTINGS')
 app = create_app(config_name)
