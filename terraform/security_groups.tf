2ff67f213feb9eaa308323f11db7b0964574d1af
Onen Julius <jonen54@gmail.com>
2018-01-28 23:39:10 +0300

    Create security groups

---
+++ terraform/security_groups.tf
@@ -0,0 +1,22 @@
+rsource "aws_security_group" "API" {
+    name = "FrontEnd"
+    tags {
+        Name = "API"
+    }
+
+    description = "Only HTTP connections inbound"
+    vpc-id = "${aws_vpc.terraformmain.id}"
+
+    ingress {
+        from_port = 80
+        to_port = 80
+        protocol = "TCP"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+    egress {
+        from_port = 0
+        to_port = 0
+        protocol = "-1"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+}
