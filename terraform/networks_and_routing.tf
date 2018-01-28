5c5b39af1e40acc061d10b456e9e4f0391610dd4
Onen Julius <jonen54@gmail.com>
2018-01-28 23:33:40 +0300

    Create access control lists

---
+++ terraform/networks_and_routing.tf
@@ -0,0 +1,32 @@
+data "aws_availability_zones" "available" {}
+
+/* EXTERNAL NETWORK, IG, ROUTE TABLE */
+ resource "aws_internet_gateway" "igw" {
+     vpc_id = "${aws_vpc.terraformmain.id}"
+     tags {
+         Name = "Terraform generated IGW for bucketlist"
+     }
+ }
+
+ resource "aws_network_acl" "all" {
+     vpc_id = "${aws_vpc.terraformmain.id}"
+     egress {
+         protocol = "-1"
+         rule_no = 2
+         action = "allow"
+         cidr_block = "0.0.0.0/0"
+         from_port = 0
+         to_port = 0
+     }
+     ingress {
+         protocol = "-1"
+         rule_no = 1
+         action = "allow"
+         cidr_block = "0.0.0.0/0"
+         from_port = 0
+         to_port = 0
+     }
+     tags {
+         Name = "Bucketlist Open ACL"
+     }
+ }
